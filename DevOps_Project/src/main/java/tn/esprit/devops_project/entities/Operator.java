a227d40cd8a58fd2ac47cf70ff3a66a3d5d2a294
houssem hosni <61200590+houssemhosni2@users.noreply.github.com>
2024-10-02 00:07:17 +0100

    Update Operator.java

--- DevOps_Project/src/main/java/tn/esprit/devops_project/entities/Operator.java
+++ DevOps_Project/src/main/java/tn/esprit/devops_project/entities/Operator.java
@@ -27,7 +27,6 @@ public class Operator implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	Long idOperateur;
 	String fname;
-	String test2;
 	String lname;
 	String password;
 	@OneToMany
